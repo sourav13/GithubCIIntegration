4086c502aea06231e915bed7646b05ca3d24601e
Sourav Sachdeva <souravsachdeva13@gmail.com>
2019-12-14 23:07:04 -0800

    added print statement to check

--- GithubCIIntegration/ViewController.swift
+++ GithubCIIntegration/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("first")
         // Do any additional setup after loading the view.
     }
 
